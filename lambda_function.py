0b321e9920271e954e65fd139775b383a4df4de5
十百千 <x1001000@users.noreply.github.com>
2022-12-06 22:02:29 +0800

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,45 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    body = event['body']
+    signature = event['headers']['x-line-signature']
+    handler.handle(body, signature)
+    @handler.add(MessageEvent, message=TextMessage)
+    def message_text(event):
+        payload = json.dumps({
+          "model": "text-davinci-003",
+          "prompt": event.message.text,
+          "temperature": 0.5,
+          "max_tokens": 150,
+          "top_p": 0.3,
+          "frequency_penalty": 0.5,
+          "presence_penalty": 0.0})
+        r = requests.post(openai_api, headers=headers, data=payload)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=json.loads(r.text)['choices'][0]['text'].strip())
+        )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+import requests
+openai_api = 'https://api.openai.com/v1/completions'
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f'Bearer {OPENAI_API_KEY}'}
