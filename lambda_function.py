2b4c9da579ab62484cd302ca3cad9ce424979179
十百千 <x1001000@users.noreply.github.com>
2023-01-07 16:44:24 +0800

    debug

--- lambda_function.py
+++ lambda_function.py
@@ -1,31 +1,3 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    body = event['body']
-    signature = event['headers']['x-line-signature']
-    handler.handle(body, signature)
-    @handler.add(MessageEvent, message=TextMessage)
-    def message_text(event):
-        payload = json.dumps({
-          "model": "text-davinci-003",
-          "prompt": event.message.text,
-          "temperature": 0.5,
-          "max_tokens": 2048,
-          "top_p": 0.3,
-          "frequency_penalty": 0.5,
-          "presence_penalty": 0.0})
-        r = requests.post(openai_api, headers=headers, data=payload)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=json.loads(r.text)['choices'][0]['text'].strip())
-        )
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -37,9 +9,36 @@ from linebot.models import (
 )
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    payload = json.dumps({
+        "model": "text-davinci-003",
+        "prompt": event.message.text,
+        "temperature": 0.5,
+        "max_tokens": 2048,
+        "top_p": 0.3,
+        "frequency_penalty": 0.5,
+        "presence_penalty": 0.0})
+    r = requests.post(openai_api, headers=headers, data=payload)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=json.loads(r.text)['choices'][0]['text'].strip())
+    )
 
 import requests
 openai_api = 'https://api.openai.com/v1/completions'
 headers = {
     "Content-Type": "application/json",
     "Authorization": f'Bearer {OPENAI_API_KEY}'}
+
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    body = event['body']
+    signature = event['headers']['x-line-signature']
+    handler.handle(body, signature)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
