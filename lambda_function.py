33c1d490ba02720fc61e424b1a3aaad3d8ffd788
十百千 <x1001000@users.noreply.github.com>
2023-01-19 11:10:03 +0000

    context as a prompt

--- lambda_function.py
+++ lambda_function.py
@@ -11,7 +11,7 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    prompt = event.message.text + '?'
+    prompt = f'GPT-1000是十百千實驗室開發的深度問答機器人，但只有2021年以前的知識，目前不提供連續問答。\n使用者問：{event.message.text}\nGPT-1000答：'
     payload = json.dumps({
         "model": "text-davinci-003",
         "prompt": prompt,
@@ -21,7 +21,7 @@ def message_text(event):
         "frequency_penalty": 0.5,
         "presence_penalty": 0.0})
     r = requests.post(openai_api, headers=headers, data=payload)
-    completion = json.loads(r.text)['choices'][0]['text'].strip()
+    completion = json.loads(r.text)['choices'][0]['text']
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=completion)
