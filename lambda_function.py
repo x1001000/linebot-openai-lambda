1569f87d3837f9d6765ffaa68d7f7810615f48a4
十百千 <x1001000@users.noreply.github.com>
2023-02-14 10:05:56 +0800

    converse by context

--- lambda_function.py
+++ lambda_function.py
@@ -11,11 +11,12 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
+    prompt = preprompt.get(event.source.user_id, 'GPT-1000是串接OpenAI API的LINE機器人，所使用的語言模型GPT-3只有2021年以前的知識，made in 十百千實驗室 by Phil Alive。\n') + f'使用者問：{event.message.text}\nGPT-1000答：'
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f'GPT-1000是串接OpenAI API的LINE機器人，所使用的語言模型GPT-3只有2021年以前的知識。GPT-1000目前不提供連續問答，made in 十百千實驗室 by Phil Alive。\n使用者問：{event.message.text}\nGPT-1000答：',
+        prompt=prompt,
         temperature=0.5,
-        max_tokens=4000,
+        max_tokens=1024,
         top_p=0.3,
         frequency_penalty=0.5,
         presence_penalty=0.0)
@@ -25,6 +26,9 @@ def message_text(event):
         event.reply_token,
         TextSendMessage(text=completion.text)
     )
+    preprompt[event.source.user_id] = f'{prompt}{completion.text}\n'
+
+preprompt = {}
 
 import openai
 openai.api_key = OPENAI_API_KEY
