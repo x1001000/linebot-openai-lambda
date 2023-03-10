0083f7e66597105375106fb265b0bb0c515a8cdc
十百千 <x1001000@users.noreply.github.com>
2023-03-10 06:33:14 +0000

    You exceeded your current quota... You can find your API key at...

--- lambda_function.py
+++ lambda_function.py
@@ -46,7 +46,8 @@ def handle_text_message(event):
             model="gpt-3.5-turbo",
             messages=preprompt + prompt)
     except (openai.error.RateLimitError, openai.error.AuthenticationError) as e:
-        openai.api_key = OPENAI_API_KEY('new')
+        if 'You' in str(e):
+            openai.api_key = OPENAI_API_KEY('new')
         requests.post(line_notify_api, headers=header, data={'message': e})
         line_bot_api.reply_message(
             event.reply_token,
